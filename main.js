18d656c925c4750b11eb38228057b59bd128da07
Abduraim Abduraimov <abduraimabduraimov@Abduraims-MacBook-Pro.local>
2024-05-31 23:54:14 +0500

    end

---
+++ main.js
@@ -0,0 +1,49 @@
+let userName = prompt("Welcome, What's is your name")
+
+let userFamily = prompt("Your family name")
+
+console.log(userFamily, userName);
+
+alert("What's up " + userName)
+
+let currentYear = 2024
+
+let userAge = prompt(userName + " Your birth year")
+
+console.log(currentYear - userAge);
+
+let userRegister = prompt("Your full information: " + "Name: " + userName + " and your age is " + (currentYear - userAge) + " to confirm type the word confirm")
+
+console.log(userRegister);
+
+let mathProblems = prompt(userName + " Do you want to solve some math problems ")
+
+console.log(mathProblems);
+
+let startPoint = prompt("If you want to start type the word go")
+
+console.log(startPoint);
+
+let firstProb = prompt(" Solve the problem " + "145 + 55 = ?")
+
+console.log("145 + 55 = 200," + " Your answer is: " + firstProb);
+
+let secondProb = prompt(" Solve the problem " + "170 - 30 = ?")
+
+console.log("170 - 30 = 140," + " Your answer is: " + secondProb);
+
+let thirdProb = prompt(" Solve the problem " + "80 * 2 = ?")
+
+console.log("80 x 2 = 160," + " Your answer is: " + thirdProb);
+
+let fourthProb = prompt(" Solve the problem " + "350 : 2 = ?") 
+
+console.log("350 : 2 = 175," + " Your answer is: " + fourthProb);
+
+let fifthProb = prompt(" what is the rest of the problem " + "50 : 6 = ?")
+
+console.log("50 : 6 = 2," + " Your answer is: " + fifthProb);
+
+alert(userName + " If you want to see your full result, go to the console section ")
+
+
